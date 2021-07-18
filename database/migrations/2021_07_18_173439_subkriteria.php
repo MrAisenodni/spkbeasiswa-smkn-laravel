b1b790eb271b535d09324d684293b6d7761ae2e4
MrAisenodni <fikrialfayed@gmail.com>
2021-07-19 00:37:27 +0700

    updated subkriteria migration

--- database/migrations/2021_07_18_173439_subkriteria.php
+++ database/migrations/2021_07_18_173439_subkriteria.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Login extends Migration
+class Subsubkriteria extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class Login extends Migration
      */
     public function up()
     {
-        // Migrasi Tabel Login
-        Schema::create('login', function (Blueprint $table) {
-            $table->id('id_login');
-            $table->bigInt('id_siswa')->nullable();
-            $table->bigInt('id_pengguna')->nullable();
-            $table->string('username', 18);
-            $table->string('password');
-            $table->rememberToken('token');
-            $table->enum('akses', ['usr', 'adm']);
+        // Migrasi Tabel Subkriteria
+        Schema::create('subkriteria', function (Blueprint $table) {
+            $table->id('id_subkriteria');
+            $table->bigInt('id_kriteria');
+            $table->string('nama', 100);
+            $table->float('bobot', 5, 2);
             $table->datetime('created_at')->nullable();
             $table->string('created_by')->nullable();
             $table->datetime('updated_at')->nullable();
@@ -38,7 +35,7 @@ class Login extends Migration
      */
     public function down()
     {
-        // Drop Tabel Login
-        Schema::dropIfExists('login');
+        // Drop Tabel Subkriteria
+        Schema::dropIfExists('subkriteria');
     }
 }
