4263259a689cb0d850fe4091fe1b9fcd6c4f07ee
MrAisenodni <fikrialfayed@gmail.com>
2021-07-18 23:18:08 +0700

    updated siste migration

---
+++ database/migrations/2021_07_18_161218_siswa.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Siswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Migrasi Tabel Siswa
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id('id_siswa');
+            $table->string('nim', 10)->unique();
+            $table->string('nama', 100);
+            $table->string('email')->unique();
+            $table->string('no_hp', 13)->unique();
+            $table->longtext('alamat');
+            $table->datetime('created_at')->nullable();
+            $table->datetime('created_by')->nullable();
+            $table->datetime('updated_at')->nullable();
+            $table->datetime('updated_by')->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->datetime('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop Tabel Siswa
+        Schema::dropIfExists('siswa');
+    }
+}
