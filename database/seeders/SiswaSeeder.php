4f03e130b47e9239b1da70323f29a01ecf3c2eb1
MrAisenodni <fikrialfayed@gmail.com>
2021-07-20 14:27:37 +0700

    updated siswa seeder

---
+++ database/seeders/SiswaSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class SiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $current_time = Carbon::now()->toDateTimeString();
+
+        return DB::table('siswa')
+                    ->insert([
+                        [
+                            'nim'       => '1710512010',
+                            'jenkel'    => 'l',
+                            'nama'      => 'Siti Fatimah',
+                            'email'     => 's.fatmagn9@gmail.com',
+                            'no_hp'     => '0895333093116',
+                            'alamat'    => 'Jl Masjid No 31',
+                            'created_at'=> $current_time,
+                            'created+by'=> 'Admin',
+                        ],
+                    ]);
+    }
+}
