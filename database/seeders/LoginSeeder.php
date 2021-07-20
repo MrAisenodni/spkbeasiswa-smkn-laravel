53d8e0bea0b791588f64275684fa8ab0792815b8
MrAisenodni <fikrialfayed@gmail.com>
2021-07-20 14:41:16 +0700

    updated LoginSeeder

--- database/seeders/LoginSeeder.php
+++ database/seeders/LoginSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class LoginSeeder extends Seeder
 {
@@ -13,6 +14,27 @@ class LoginSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Data Dummy Table login
+        $current_time = Carbon::now()->toDateTimeString();
+
+        return DB::table('login')
+                    ->insert([
+                        [
+                            'id_siswa'      => 1,
+                            'username'      => 'user',
+                            'password'      =>  Hash::make('user'),
+                            'akses'         => 'usr',
+                            'created_at'    => $current_time,
+                            'created_by'    => 'Admin',
+                        ],
+                        [
+                            'id_pengguna'   => 1,
+                            'username'      => 'admin',
+                            'password'      => Hash::make('admin'),
+                            'akses'         => 'adm',
+                            'created_at'    => $current_time,
+                            'created_by'    => 'Admin'
+                        ]
+                    ]);
     }
 }
